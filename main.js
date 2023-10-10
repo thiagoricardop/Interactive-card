6d14ebefcbd5faa123e6caa58af4eb2eb0d057fe
thiagoricarop <t.ricardooliveira08@gmail.com>
2023-10-09 22:20:14 -0300

    update

--- main.js
+++ main.js
@@ -6,3 +6,15 @@ function updateNumber() {
     var number = document.getElementById('Card_num').value;   // get input value
     document.getElementById('updatedNumber').textContent = number;   // update text
 }
+function updateDateMonth() {
+    var number = document.getElementById('CardDate_month').value;   // get input value
+    document.getElementById('updatedDateMonth').textContent = number;   // update text
+}
+function updateDateYear() {
+    var number = document.getElementById('CardDate_year').value;   // get input value
+    document.getElementById('updatedDateYear').textContent = number;   // update text
+}
+function updateCVC() {
+    var number = document.getElementById('Card_cvc').value;   // get input value
+    document.getElementById('updatedCVC').textContent = number;   // update text
+}
