ef77784442f4f9ea8156b41367115e40692424eb
thiagoricarop <t.ricardooliveira08@gmail.com>
2023-10-09 22:02:23 -0300

    Criando js e melhorias no design

---
+++ main.js
@@ -0,0 +1,8 @@
+function updateName() {
+    var name = document.getElementById('Cardholder_Nam').value;   // get input value
+    document.getElementById('updatedName').textContent = name;   // update text
+}
+function updateNumber() {
+    var number = document.getElementById('Card_num').value;   // get input value
+    document.getElementById('updatedNumber').textContent = number;   // update text
+}
